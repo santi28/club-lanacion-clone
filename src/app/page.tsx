231b2a8c77fe6d7312ac947e9b52679256e3bd0a
Santiago de Nicolás <santidenicolas@gmail.com>
2024-09-18 15:29:23 -0300

    removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import Slider from "./components/Slider";
 import Header from "./components/Header";
 
-import { IconChevronLeft } from "./components/icons/IconChevronLeft";
-import { IconChevronRight } from "./components/icons/IconChevronRight";
 import DiscountSection from "./components/TurismoSection";
-import VoucherCard from "./components/VoucherCard";
 import VoucherSection from "./components/VoucherSection";
 
 export default function Home() {
