74d698f42ad89b68afcf6efd4e0affc4649cc593
Santiago de Nicolás <santidenicolas@gmail.com>
2024-09-18 10:44:08 -0300

    Refactor sections and add discount and voucher components

---
+++ src/app/components/sections/DiscountsSection.tsx
@@ -0,0 +1,20 @@
+import { CardCarousel } from "@/app/page";
+
+export default function DiscountSection() {
+  return (
+    <section className="mx-auto py-8 md:py-12 lg:py-16 px-4 md:px-8 lg:px-12 items-center">
+      <header className="flex justify-between items-center mb-6">
+        <div className="flex flex-col">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800">
+            Turismo por Buenos Aires
+          </h2>
+        </div>
+        <button className="font-sans border-blue-600 border-2 text-blue-600 px-4 py-2 rounded-full text-sm font-semibold hover:bg-blue-600 hover:text-white transition-colors">
+          TODOS LOS BENEFICIOS
+        </button>
+      </header>
+
+      <CardCarousel />
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import photo from "../public/images/slider.jpg";
 import { IconMapPin } from "./components/icons/IconMapPin";
 import { IconChevronLeft } from "./components/icons/IconChevronLeft";
 import { IconChevronRight } from "./components/icons/IconChevronRight";
+import DiscountSection from "./components/sections/DiscountsSection";
+import VoucherSection from "./components/sections/VoucherSection";
 
 export default function Home() {
   return (
@@ -13,70 +15,10 @@ export default function Home() {
       <Header />
       <main>
         <Slider />
-        <CardCarouselSection />
+        <DiscountSection />
+        <VoucherSection />
       </main>
     </>
-  );
-}
-
-export function DiscountCarouselSection() {
-  <section className="mx-auto py-8 md:py-12 lg:py-16 px-4 md:px-8 lg:px-12">
-    <header className="flex justify-between items-center mb-6">
-      <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800">
-        Turismo por Buenos Aires
-      </h2>
-      <button className="border-blue-600 border-2 text-blue-600 px-4 py-2 rounded-full text-sm font-semibold hover:bg-blue-600 hover:text-white transition-colors">
-        TODOS LOS BENEFICIOS
-      </button>
-    </header>
-
-    {/* Carrusel de tarjetas con 4 visibles */}
-    <div className="flex items-center gap-4">
-      <button
-        aria-label="Anterior"
-        className="text-stone-950 hover:bg-stone-100 p-2 rounded-full"
-      >
-        <IconChevronLeft className="w-6 h-6" />
-      </button>
-
-      {/* Contenedor de las tarjetas */}
-      <div className="flex flex-grow gap-4">
-        <DiscountBenefitCard />
-        <DiscountBenefitCard />
-        <DiscountBenefitCard />
-        <DiscountBenefitCard />
-      </div>
-
-      <button
-        aria-label="Siguiente"
-        className="text-stone-950 hover:bg-stone-100 p-2 rounded-full"
-      >
-        <IconChevronRight className="w-6 h-6" />
-      </button>
-    </div>
-  </section>;
-}
-
-export function CardCarouselSection() {
-  return (
-    <section className="mx-auto py-8 md:py-12 lg:py-16 px-4 md:px-8 lg:px-12 bg-neutral-100 items-center">
-      <header className="flex justify-between items-center mb-6">
-        <div className="flex flex-col">
-          <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800">
-            Turismo por Buenos Aires
-          </h2>
-          <p className="text-sm text-gray-600">
-            ¿Sos socio de Club LA NACION? Descargá tu código y disfrutá
-            beneficios exclusivos en tus marcas favoritas
-          </p>
-        </div>
-        <button className="font-sans border-blue-600 border-2 text-blue-600 px-4 py-2 rounded-full text-sm font-semibold hover:bg-blue-600 hover:text-white transition-colors">
-          TODOS LOS BENEFICIOS
-        </button>
-      </header>
-
-      <CardCarousel />
-    </section>
   );
 }
 
